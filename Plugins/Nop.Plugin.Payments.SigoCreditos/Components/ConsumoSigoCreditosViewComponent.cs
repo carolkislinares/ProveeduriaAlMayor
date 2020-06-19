4551c4ba3b73e77c4b6cca1aaabad47693618686
CLinares <CLinares@LTFABSIS021>
2020-06-19 11:40:38 -0400

    plugins sigocreditos

---
+++ Plugins/Nop.Plugin.Payments.SigoCreditos/Components/ConsumoSigoCreditosViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nop.Plugin.Payments.SigoCreditos.Components {
+    [ViewComponent(Name = "ConsumoSigoCreditos")]
+    public class ConsumoSigoCreditosViewComponent : NopViewComponent {
+
+        public ConsumoSigoCreditosViewComponent() {
+        }
+
+        public IViewComponentResult Invoke() {
+
+            
+            return View("~/Plugins/Payments.SigoCreditos/Views/_ConsumoSigoCreditos.cshtml");
+        }
+    }
+}
